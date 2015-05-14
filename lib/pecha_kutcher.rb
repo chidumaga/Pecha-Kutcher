42ff6bc26cc928ca3d4b6319f3d2f49a7e6a4f38
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-14 19:37:22 +0100

    App working, basic functionality

--- lib/pecha_kutcher.rb
+++ lib/pecha_kutcher.rb
@@ -1,16 +1,21 @@
 require 'sinatra/base'
+require './lib/petchakutcher'
 
 class Pecha_Kutcher < Sinatra::Base
   get '/' do
+    @@pechkuch = PetchaKutcher.new
     erb :index
   end
 
   post '/' do
     @name = params[:name]
+    @@pechkuch.add_participant params[:name], params[:subject]
     erb :index
   end
 
   get '/next' do
+    @return_text = @@pechkuch.pick_next
+    erb :next
   end
 
   # start the server if ruby file executed directly
---
+++ lib/petchakutcher.rb
@@ -0,0 +1,82 @@
+class Participant
+
+  def initialize name, subject
+    @name = name
+    @name[0] = @name[0].capitalize
+    @subject = subject
+    @subject[0] = @subject[0].capitalize
+  end
+
+attr_accessor :name
+attr_accessor :subject #why are these attr_accessors
+
+end
+
+class PetchaKutcher
+
+  def initialize
+    @pk = []
+  end
+
+
+  def add_participant name, subject
+    particiant1 = Participant.new name, subject
+    @pk << particiant1
+    @pk.shuffle!
+  end
+
+
+  def list_participants
+    p self
+  end
+
+
+  def save_participants(filename)
+    txt = open(filename,'w')
+    @pk.each { |particiant1|  txt.write("#{particiant1.name}%#{particiant1.subject}\n") }
+    txt.close
+  end
+
+
+  def load_participants(filename)
+    txt = open(filename)
+    while !txt.eof?
+      a, b = txt.readline.split("%")
+      b = b[0..-3]  #get rid of new line chars
+      particiant1 = Participant.new a, b
+      @pk << particiant1
+    end
+    @pk.shuffle
+  end
+
+
+  #returns one string of whos up next
+  def pick_next
+    if @pk.count == 0
+      "No more entrants"
+    else
+      particiant1 = @pk.pop
+      interesting = ["interesting","boring","enlightening","absorbing","fascinating","gripping","compulsive","mind numbing"].shuffle.last
+      person = ["delectable","mighty","sexy","impressive","lazy","wistful"].shuffle.last
+      talking = ["talking","whaffling on","preaching on","prattling on","jabbering on ","bleating on"].shuffle.last
+      "Up next we have the #{person} #{particiant1.name} #{talking} about the very #{interesting} subject of #{particiant1.subject}."
+    end
+  end
+
+end
+
+
+pk1 = PetchaKutcher.new
+# pk1.add_participant("tem","lewisham")
+# pk1.add_participant("ptolemy","bubbles")
+# pk1.add_participant("chedu","shirts")
+
+# # #pk1.list_participants
+# pk1.save_participants("pk.txt")
+
+# pk1.load_participants("pk.txt")
+
+# p pk1.pick_next
+# p pk1.pick_next
+# p pk1.pick_next
+# p pk1.pick_next
